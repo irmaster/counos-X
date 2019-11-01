608a2a92b93b9b9a7172fd2b7c68f133867a5ae7
Counos Dev <dev@counos.io>
2019-11-01 16:27:04 +0330

    edit default port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -33,11 +33,11 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 10841);
+        return MakeUnique<CBaseChainParams>("", 11841);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet4", 10821);
+        return MakeUnique<CBaseChainParams>("testnet4", 11821);
     else if (chain == CBaseChainParams::REGTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 10822);
+        return MakeUnique<CBaseChainParams>("regtest", 11822);
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
